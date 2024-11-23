27cee386817c686cddc367afaa5f130d2391b98d
Anastasiia <nastya21622162@gmail.com>
2024-11-23 15:27:08 +0300

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,8 +0,0 @@
-# Парктическое задание "Переменные"
-number_of_homeworks = 12
-number_of_hours = 1.5
-name_of_course = "Python"
-time_for_one_homework = number_of_hours / number_of_homeworks
-
-print ('Курс: ', name_of_course, ',', 'всего задач: ', number_of_homeworks,',', 'затрачено часов: ', number_of_hours, ',', 
-       'среднее время выполнения: ', time_for_one_homework)
